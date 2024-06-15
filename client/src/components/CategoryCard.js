3ce3c4dc610c966b69950ecf5946412bafd68683
da_b1rmuda <lamurka1337@yandex.ru>
2024-06-15 21:14:41 +0400

    fix image category

--- client/src/components/CategoryCard.js
+++ client/src/components/CategoryCard.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 
 export class CategoryCard extends Component {
-
-  render() {
-    return (
-      <div onClick={() => this.props.small ? this.props.changeCurrentCategory(this.props.category.category_id) : null} style={{ backgroundImage: "linear-gradient(rgba(0,0,0,0.1),rgba(0,0,0,0.5)), url(/img/" + this.props.category.category_image }} className={this.props.small ? "SmallCategoryCard" : "CategoryCard"}>
-        <p class="CategoryText">{this.props.category.category_name}</p>
-      </div>
-    )
-  }
+	capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1)
+	}
+	render() {
+		return (
+			<div
+				onClick={() =>
+					this.props.small
+						? this.props.changeCurrentCategory(this.props.category.category_id)
+						: null
+				}
+				style={{
+					backgroundImage:
+						'linear-gradient(rgba(0,0,0,0.1),rgba(0,0,0,0.5)), url(/img/' +
+						capitalizeFirstLetter(this.props.category.category_image),
+				}}
+				className={this.props.small ? 'SmallCategoryCard' : 'CategoryCard'}
+			>
+				<p class='CategoryText'>{this.props.category.category_name}</p>
+			</div>
+		)
+	}
 }
 
 export default CategoryCard
